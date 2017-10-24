7aec4003846cfa9a5a81088ef3ab3efd8caa7c2f
saumya <saumyawardhan1@gmail.com>
2017-10-24 12:20:42 +0530

    Divisor Of factorial

--- eminence/tail.cpp
+++ eminence/tail.cpp
@@ -18,7 +18,19 @@ int factorial(int num, int result=1){
 }
 
 int main(){
-  int x = factorial(12);
-  cout<<x<<endl;
+  // int x = factorial(12);
+  // cout<<x<<endl;
+  int n;
+  cin >> n;
+  int a[n];
+  for(int i=0; i<n; i++){
+    cin >> a[i];
+  }
+
+  for(int i=n-1; i>=0; i--){
+    cout << a[i] << " ";
+  }
+
+  cout << endl;
   return 0;
 }
